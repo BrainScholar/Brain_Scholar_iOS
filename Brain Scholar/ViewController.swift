56ee5a02ec2c1a09dfbdd5632c7f1368564ac9fc
derekroberts <derek.roberts@kysu.edu>
2017-07-05 14:11:19 -0400

    5/7/17 Working picker view, sliders and button.

--- Brain Scholar/ViewController.swift
+++ Brain Scholar/ViewController.swift
@@ -8,8 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
 
+    var graphSpeeds = ["Very Fast", "Fast", "Moderate", "Slow", "Very Slow"]
+    @IBOutlet weak var gnaValue: UILabel!
+    @IBOutlet weak var gkValue: UILabel!
+    @IBOutlet weak var betaValue: UILabel!
+    @IBOutlet weak var gammaValue: UILabel!
+    @IBOutlet weak var v_stimValue: UILabel!
+    @IBOutlet weak var cValue: UILabel!
+    
+    @IBOutlet weak var gnaSlider: UISlider!
+    @IBOutlet weak var gkSlider: UISlider!
+    @IBOutlet weak var betaSlider: UISlider!
+    @IBOutlet weak var gammaSlider: UISlider!
+    @IBOutlet weak var v_stimSlider: UISlider!
+    @IBOutlet weak var cSlider: UISlider!
+    
+    @IBAction func gnaValueChanged(_ sender: UISlider) {
+        gnaValue.text = "\(gnaSlider.value)"
+    }
+    
+    @IBAction func gkValueChanged(_ sender: UISlider) {
+        gkValue.text = "\(gkSlider.value)"
+    }
+    
+    @IBAction func betaValueChanged(_ sender: UISlider) {
+    }
+    
+    
+    @IBAction func gammaValueChanged(_ sender: UISlider) {
+        gammaValue.text = "\(gammaSlider.value)"
+    }
+    
+    @IBAction func v_stimValueChanged(_ sender: UISlider) {
+        v_stimValue.text = "\(v_stimSlider.value)"
+    }
+    
+    @IBAction func cValueChanged(_ sender: UISlider) {
+        cValue.text = "\(cSlider.value)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +60,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int{
+        return 1
+    }
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return graphSpeeds.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return graphSpeeds[row]
+    }
 }
 
